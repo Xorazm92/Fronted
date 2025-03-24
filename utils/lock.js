b3b223be1406f16495a80ac650fb865a9d259073
Xorazm92 <rayadunina2021@gmail.com>
2025-03-24 12:07:03 +0500

    auth to next

---
+++ utils/lock.js
@@ -0,0 +1,24 @@
+/* eslint global-require: 0 */
+
+const getLock = (options) => {
+  const CLIENT_ID = '';
+  const CLIENT_DOMAIN = '';
+  const Auth0Lock = require('auth0-lock').default;
+  return new Auth0Lock(CLIENT_ID, CLIENT_DOMAIN, options);
+};
+
+const getBaseUrl = () => `${window.location.protocol}//${window.location.host}`;
+
+const getOptions = () => ({
+  closable: false,
+  auth: {
+    responseType: 'token',
+    redirectUrl: `${getBaseUrl()}/auth/signed-in`,
+    params: {
+      scope: 'openid profile email',
+    },
+  },
+});
+
+export const show = () => getLock(getOptions()).show();
+export const logout = () => getLock().logout({ returnTo: getBaseUrl() });
