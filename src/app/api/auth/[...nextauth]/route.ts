0c51152ccc9d5f16b52b44fcb8f82c00a5f43ad1
Xorazm92 <rayadunina2021@gmail.com>
2025-03-25 11:03:12 +0500

    Next google  auth
    git rm --cached .env
    git commit --amend --no-edit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET, // Qo‘shimcha xavfsizlik uchun
+});
+
+export { handler as GET, handler as POST };
